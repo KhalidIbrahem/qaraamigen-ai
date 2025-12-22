9958aca36dadab9ea87301c24b41ea3372a905cd
khalid300 <ibrahimkhalid032@gmail.com>
2025-12-21 22:36:51 -0600

    Fixed

--- frontend/components/providers/SessionProvider.tsx
+++ frontend/components/providers/SessionProvider.tsx
@@ -2,10 +2,13 @@
 
 import { SessionProvider as Provider } from "next-auth/react";
 
-export default function SessionProvider({ children }: { children: React.ReactNode }) {
+export default function SessionProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   // 1. Assign to a new capitalized variable to bypass the type error
   const AnyProvider = Provider as any;
-
 
   return <AnyProvider>{children}</AnyProvider>;
 }
